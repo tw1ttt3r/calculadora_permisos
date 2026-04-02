f7f558b0429b0475ad089f8534c10d3a8219dbc6
tw1ttt3r <tw1ttt3r@gmail.com>
2026-04-01 19:03:46 -0600

    feat: added new functionalities

--- src/config/init.config.mjs
+++ src/config/init.config.mjs
@@ -1,10 +1,56 @@
+import { updateTabStore } from "./store.mjs"
+import { getElements } from "./utility.mjs"
+
 function init() {
   document.title = "Calculadora de Permisos"
 }
 
+function buildTabs() {
+  getElements("tab")
+    .forEach((k, p) => {
+      if (!p) {
+        k.classList.add("font-bold")
+      }
+      k.addEventListener("click", () => {
+        updateTabStore(p)
+        setSelectionTab(p)
+        activeCalculator(p)
+      })
+    })
+
+    getElements("container-calculator")
+    .forEach((k, p) => {
+      if (!p) {
+        k.classList.add("container-calculator-visible")
+        return
+      }
+      k.classList.add("container-calculator-invisible")
+    })
+}
+
+function activeCalculator(i) {
+  getElements("container-calculator")
+    .forEach((k, p) => {
+      if (i !== p) {
+        k.classList.remove("container-calculator-visible")
+        k.classList.add("container-calculator-invisible")
+      } else {
+        k.classList.remove("container-calculator-invisible")
+        k.classList.add("container-calculator-visible")
+      }
+    })
+}
+
+function setSelectionTab(i) {
+  const e = getElements("tab")
+    e.forEach((k, p) => {
+      k.classList.remove("font-bold")
+      if (p === i) k.classList.add("font-bold")
+    })
+}
+
 function buildControls(permisos) {
-  document
-    .querySelectorAll(".permisos-control")
+  getElements("permisos-control")
     .forEach((k) => {
       k.append(...buildOptions(permisos))
       initValueControl(k, 0, permisos)
@@ -14,13 +60,13 @@ function buildControls(permisos) {
         const { value: v } = permisos[value]
         switch(id) {
           case "controls-user":
-            document.querySelectorAll(".controls-user-description")[0].textContent = v
+            getElements("controls-user-description")[0].textContent = v
             break;
           case "controls-group":
-            document.querySelectorAll(".controls-group-description")[0].textContent = v
+            getElements("controls-group-description")[0].textContent = v
             break;
           case "controls-others":
-            document.querySelectorAll(".controls-others-description")[0].textContent = v
+            getElements("controls-others-description")[0].textContent = v
             break;
         }
       })
@@ -30,9 +76,9 @@ function buildControls(permisos) {
 function initValueControl(c, p, permisos) {
   c.setAttribute("selectedIndex", p.toString())
   const { value: v } = permisos[p]
-  document.querySelectorAll(".controls-user-description")[p].textContent = v
-  document.querySelectorAll(".controls-group-description")[p].textContent = v
-  document.querySelectorAll(".controls-others-description")[p].textContent = v
+  getElements("controls-user-description")[p].textContent = v
+  getElements("controls-group-description")[p].textContent = v
+  getElements("controls-others-description")[p].textContent = v
 }
 
 function buildOptions(permisos) {
@@ -48,5 +94,6 @@ function buildOptions(permisos) {
 
 export {
   init,
-  buildControls
+  buildControls,
+  buildTabs
 }
